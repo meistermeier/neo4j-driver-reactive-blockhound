00aef78a12703acc3baa02e0551b445251aa1eba
Gerrit Meier <meistermeier@gmail.com>
2023-06-30 12:25:32 +0200

    Add logging

--- src/main/java/com/meistermeier/neo4j/driver/ReactiveBlocker.java
+++ src/main/java/com/meistermeier/neo4j/driver/ReactiveBlocker.java
@@ -1,8 +1,10 @@
 package com.meistermeier.neo4j.driver;
 
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Logging;
 
 /**
  * @author Gerrit Meier
@@ -10,6 +12,8 @@ import org.neo4j.driver.GraphDatabase;
 public class ReactiveBlocker {
 
 	public static Driver getDriver() {
-		return GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "verysecret"));
+		return GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "verysecret"), Config.builder()
+			.withLogging(Logging.slf4j())
+			.build());
 	}
 }
